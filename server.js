0199f875af79dc220d78de1680ca1c4cb3768faf
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-18 09:11:34 +0200

    fixed mongoose depreciation warning

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = require('./config/keys').mongoURI;
 mongoose
   .connect(
     db,
-    { useNewUrlParser: true }
+    { useNewUrlParser: true, useCreateIndex: true }
   )
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
