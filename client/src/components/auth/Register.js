80b338c0211db12b287bc20b0099fdaafc03b71b
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-21 18:38:00 +0200

    onSubmit  API call

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Register extends Component {
   state = {
@@ -17,7 +18,11 @@ export class Register extends Component {
     e.preventDefault();
     const { name, email, password, password2 } = this.state;
     const newUser = { name, email, password, password2 };
-    console.log(newUser);
+
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
   };
 
   render() {
