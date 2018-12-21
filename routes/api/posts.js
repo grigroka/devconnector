441000b9175705d7919a655b8cb030a2e5e452a0
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-21 10:20:37 +0200

    delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,6 +7,8 @@ const passport = require('passport');
 
 // Load Post model
 const Post = require('../../models/Post');
+// Load Profile model
+const Profile = require('../../models/Profile');
 // Load Validation
 const validatePostInput = require('../../validation/post');
 
@@ -57,6 +59,31 @@ router.post(
       user: req.user.id
     });
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: 'User not authorized' });
+          }
+
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    });
   }
 );
 
