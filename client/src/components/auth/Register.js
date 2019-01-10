4ccf54a6fda88f0984973924fdbefbd295c34659
Rokas Grigaravicius <grigroka@gmail.com>
2019-01-10 11:10:56 +0200

    test dispatch

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 export class Register extends Component {
   state = {
@@ -20,10 +22,11 @@ export class Register extends Component {
     const { name, email, password, password2 } = this.state;
     const newUser = { name, email, password, password2 };
 
-    axios
-      .post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }));
+    this.props.registerUser(newUser);
+    // axios
+    //   .post('/api/users/register', newUser)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => this.setState({ errors: err.response.data }));
   };
 
   render() {
@@ -113,4 +116,7 @@ export class Register extends Component {
   }
 }
 
-export default Register;
+export default connect(
+  null,
+  { registerUser }
+)(Register);
