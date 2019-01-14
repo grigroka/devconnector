c64079a359e81bb07dbb75277aa2c8a51eee524f
Rokas Grigaravicius <grigroka@gmail.com>
2019-01-14 17:59:46 +0200

    clear current profile

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -29,7 +31,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
     // Redurect to login
     window.location.href = '/login';
   }
