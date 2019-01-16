7d5fb06a3daa39965aa1329ecbcadfca7663efcf
Rokas Grigaravicius <grigroka@gmail.com>
2019-01-16 11:12:17 +0200

    Add Experience form

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credencials/AddExperience';
 
 import './App.css';
 
@@ -63,6 +64,11 @@ class App extends Component {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
               />
             </div>
             <Footer />
