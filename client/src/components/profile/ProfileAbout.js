cbf2901954bad181f333525b0f8bf4949466b30c
Rokas Grigaravicius <grigroka@gmail.com>
2019-01-23 11:29:03 +0200

    Profile subcomponents

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class ProfileAbout extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TODO: Profile about</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileAbout;
