f31834558ac2b6e949c39a38f3a8514de5b7e0e8
Rokas Grigaravicius <grigroka@gmail.com>
2019-01-24 18:21:43 +0200

    resolved landing page and not found page issue

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,10 @@ import { clearCurrentProfile } from './actions/profileActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import PrivateRoute from './components/common/PrivateRoute';
-
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/create-profile/CreateProfile';
-import EditProfile from './components/edit-profile/EditProfile';
-import AddExperience from './components/add-credencials/AddExperience';
-import AddEducation from './components/add-credencials/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import NotFound from './components/not-found/NotFound';
+import NonLandingPages from './components/common/NonLandingPages';
 
 import './App.css';
 
@@ -54,38 +43,10 @@ class App extends Component {
         <Router>
           <div className="App">
             <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Landing} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/profiles" component={Profiles} />
-                <Route exact path="/profile/:handle" component={Profile} />
-                <Route exact path="/not-found" component={NotFound} />
-                <PrivateRoute exact path="/dashboard" component={Dashboard} />
-                <PrivateRoute
-                  exact
-                  path="/create-profile"
-                  component={CreateProfile}
-                />
-                <PrivateRoute
-                  exact
-                  path="/edit-profile"
-                  component={EditProfile}
-                />
-                <PrivateRoute
-                  exact
-                  path="/add-experience"
-                  component={AddExperience}
-                />
-                <PrivateRoute
-                  exact
-                  path="/add-education"
-                  component={AddEducation}
-                />
-                <Route path="*" component={NotFound} />
-              </Switch>
-            </div>
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route component={NonLandingPages} />
+            </Switch>
             <Footer />
           </div>
         </Router>
---
+++ client/src/components/common/NonLandingPages.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import PrivateRoute from './PrivateRoute';
+import Register from '../auth/Register';
+import Login from '../auth/Login';
+import Dashboard from '../dashboard/Dashboard';
+import CreateProfile from '../create-profile/CreateProfile';
+import EditProfile from '../edit-profile/EditProfile';
+import AddExperience from '../add-credencials/AddExperience';
+import AddEducation from '../add-credencials/AddEducation';
+import Profiles from '../profiles/Profiles';
+import Profile from '../profile/Profile';
+import NotFound from '../not-found/NotFound';
+
+export class NonLandingPages extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profiles" component={Profiles} />
+          <Route exact path="/profile/:handle" component={Profile} />
+          <Route exact path="/not-found" component={NotFound} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <PrivateRoute
+            exact
+            path="/create-profile"
+            component={CreateProfile}
+          />
+          <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+          <PrivateRoute
+            exact
+            path="/add-experience"
+            component={AddExperience}
+          />
+          <PrivateRoute exact path="/add-education" component={AddEducation} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default NonLandingPages;
